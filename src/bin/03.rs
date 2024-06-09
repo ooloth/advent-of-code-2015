6ed463fed2b22b7ce75a70313a140064b436638d
Michael Uloth <hello@michaeluloth.com>
2024-06-08 23:07:16 -0400

    day 3, part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -23,8 +23,34 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(coords.len() as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut coords: HashSet<(i32, i32)> = HashSet::new();
+    coords.insert((0, 0));
+
+    let mut santa = (0, 0);
+    let mut robot = (0, 0);
+
+    input.chars().enumerate().for_each(|(i, c)| {
+        let is_santas_move = i % 2 == 0;
+
+        let mover = if is_santas_move {
+            &mut santa
+        } else {
+            &mut robot
+        };
+
+        match c {
+            '^' => mover.1 += 1,
+            'v' => mover.1 -= 1,
+            '<' => mover.0 -= 1,
+            '>' => mover.0 += 1,
+            _ => (),
+        }
+
+        coords.insert(*mover);
+    });
+
+    Some(coords.len() as u32)
 }
 
 #[cfg(test)]
@@ -33,10 +59,10 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let directions_to_number_of_houses_visited =
+        let directions_to_num_houses_visited =
             vec![(">", Some(2)), ("^>v<", Some(4)), ("^v^v^v^v^v", Some(2))];
 
-        for (input, expected) in directions_to_number_of_houses_visited {
+        for (input, expected) in directions_to_num_houses_visited {
             let result = part_one(input);
             assert_eq!(result, expected);
         }
@@ -44,9 +70,10 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let examples = vec![("abc", Some(1)), ("def", Some(1)), ("ghi", Some(1))];
+        let directions_to_num_houses_visited =
+            vec![("^v", Some(3)), ("^>v<", Some(3)), ("^v^v^v^v^v", Some(11))];
 
-        for (input, expected) in examples {
+        for (input, expected) in directions_to_num_houses_visited {
             let result = part_two(input);
             assert_eq!(result, expected);
         }
