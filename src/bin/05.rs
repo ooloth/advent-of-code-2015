2842f0b9af6b14c285cbb0d0cae3255c323ab51a
Michael Uloth <hello@michaeluloth.com>
2024-06-09 16:35:21 -0400

    day 5, part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -22,6 +22,54 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .filter(|line| {
+                let chars: Vec<char> = line.chars().collect();
+                let len = chars.len();
+
+                // Check for the same letter sandwiching another letter
+                let has_letter_sandwich = chars.windows(3).any(|w| w[0] == w[2]);
+
+                // If no letter sandwich is found, this string is not nice
+                if !has_letter_sandwich {
+                    return false;
+                }
+
+                // Store pairs and their indices so we can look for repeating, non-overlapping pairs
+                let mut pairs_with_indices: Vec<((char, char), usize)> = Vec::new();
+                let mut has_repeating_non_overlapping_pair = false;
+
+                // Iterate over all pairs of characters in the string
+                for i in 0..len - 1 {
+                    let pair = (chars[i], chars[i + 1]);
+
+                    // Check if we already found this pair at least two indices ago (non-overlapping)
+                    match pairs_with_indices
+                        .iter()
+                        .find(|&&(p, index)| p == pair && i - index > 1)
+                    {
+                        Some(_) => {
+                            // If so, this string is nice, so we can break and return early
+                            has_repeating_non_overlapping_pair = true;
+                            break;
+                        }
+                        _ => {
+                            // Otherwise, add this pair to the list of pairs and their indices and continue
+                            pairs_with_indices.push((pair, i));
+                        }
+                    }
+                }
+
+                // Return the result
+                has_repeating_non_overlapping_pair
+            })
+            .count() as u32,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,4 +94,22 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_part_two() {
+        let strings_to_num_nice = vec![
+            ("qjhvhtzxzqqjkmpb", Some(1)), // has a pair that repeats (qj) + letters that repeat with one letter between them (hvh, zxz)
+            ("xxyxx", Some(1)), // has a pair that repeats (xx) + a letter that repeats with one letter between (xyx)
+            ("uurcxstgmygtbstg", Some(0)), // has a pair that repeats (tg), but no letter that repeats with one letter between
+            ("ieodomkazucvgmuy", Some(0)), // has a letter that repeats with one letter between (odo), but no pair that repeats
+            (
+                "qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazucvgmuy",
+                Some(1 + 1 + 0 + 0),
+            ),
+        ];
+
+        for (input, expected) in strings_to_num_nice {
+            let result = part_two(input);
+            assert_eq!(result, expected);
+        }
+    }
 }
