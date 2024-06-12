c445e2145993ae2da6056778946330cb9c55fe5d
Michael Uloth <hello@michaeluloth.com>
2024-06-11 20:32:21 -0400

    day 6, part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -57,8 +57,56 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(lights.concat().iter().filter(|&&x| x).count())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    // Allocate a 2D vector this time to avoid a stack overflow
+    let mut lights = vec![vec![0; 1000]; 1000];
+
+    input.lines().for_each(|line| {
+        let mut parts = line.split_whitespace();
+        let action = parts.next().unwrap();
+
+        match action {
+            "turn" => {
+                let state = parts.next().unwrap();
+                let (start_x, start_y) = parse_coords(parts.next().unwrap());
+                let (end_x, end_y) = parse_coords(parts.last().unwrap());
+
+                match state {
+                    "on" => {
+                        for x in start_x..=end_x {
+                            for y in start_y..=end_y {
+                                lights[x][y] += 1;
+                            }
+                        }
+                    }
+                    "off" => {
+                        for x in start_x..=end_x {
+                            for y in start_y..=end_y {
+                                if lights[x][y] > 0 {
+                                    lights[x][y] -= 1;
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            "toggle" => {
+                let (start_x, start_y) = parse_coords(parts.next().unwrap());
+                let (end_x, end_y) = parse_coords(parts.last().unwrap());
+
+                for x in start_x..=end_x {
+                    for y in start_y..=end_y {
+                        lights[x][y] += 2
+                    }
+                }
+            }
+            _ => {}
+        }
+    });
+
+    // Flatten the 2D array and count the number of lights that are on
+    Some(lights.concat().iter().sum())
 }
 
 #[cfg(test)]
@@ -81,9 +129,12 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let examples = vec![("abc", Some(1)), ("def", Some(1)), ("ghi", Some(1))];
+        let instruction_to_increase_in_brightness = vec![
+            ("turn on 0,0 through 0,0", Some(1)),
+            ("toggle 0,0 through 999,999", Some(2_000_000)),
+        ];
 
-        for (input, expected) in examples {
+        for (input, expected) in instruction_to_increase_in_brightness {
             let result = part_two(input);
             assert_eq!(result, expected);
         }
